4f4a1a430a9b158751668a5766a2b14cd83c6fac
Sahar <saharamerheb@gmail.com>
2023-11-17 21:33:29 +0200

    fix search

--- app/(website)/search/page.js
+++ app/(website)/search/page.js
@@ -4,6 +4,7 @@ import { useSearchParams } from "next/navigation";
 import SearchPage from "./default";
 import { searchAllPosts } from "@/lib/sanity/client";
 
+// eslint-disable-next-line @next/next/no-async-client-component
 export default async function IndexPage() {
   const searchParams = useSearchParams();
   const query = searchParams.get("q");
@@ -16,3 +17,18 @@ export default async function IndexPage() {
 }
 
 // export const revalidate = 60;
+// async function getParams(req: Request) {
+//   const stateSymbol = Object.getOwnPropertySymbols(req).find(
+//     symbol => String(symbol) === "Symbol(state)"
+//   );
+//   if (stateSymbol) {
+//     const stateValue = req[stateSymbol];
+//     const searchParams = stateValue.url.searchParams;
+
+//     const type = searchParams.get("type") || "post";
+//     const secret = searchParams.get("secret");
+//     const slug = searchParams.get("slug");
+//     console.log(type, slug, secret);
+//     return { secret, type, slug };
+//   }
+// }
